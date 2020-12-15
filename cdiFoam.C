71ca5e535832c13e35a636789eaad3fc485e3b51
Ali Bozorgzadeh <abzgzh@gmail.com>
2020-12-15 10:11:40 +0330

    Initial commit

---
+++ cdiFoam.C
@@ -0,0 +1,71 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    scalarTransportFoamCdi
+
+Description
+    Solves the steady or transient transport equation for a passive scalar.
+    Here the passive scalars are concentration and electrical potential.
+
+\*---------------------------------------------------------------------------*/
+
+// Including some class definition and libraries ( Header files )
+#include "fvCFD.H"
+#include "simpleControl.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+	// Creating objects from the above defined classes
+	// These .H files are basically code snippets ( "inclusion" files )
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    simpleControl simple(mesh);
+
+    #include "createFields.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nCalculating scalar transport\n" << endl;
+
+    Info<< "Time = " << runTime.timeName() << nl << endl;
+
+    fvScalarMatrix cEqn
+    (
+        fvm::ddt(c)
+      - fvm::laplacian(De, c)
+    );
+
+    cEqn.relax();
+    cEqn.solve();
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
