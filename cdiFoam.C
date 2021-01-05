42ba96b185feae9f8e3100808f7bbf6934c31e70
Ali Bozorgzadeh <abzgzh@gmail.com>
2021-01-05 07:37:10 +0330

    cdiFoam: Add Execution and clock time...
    ...(copied form icoFoam solver)

--- cdiFoam.C
+++ cdiFoam.C
@@ -69,6 +69,9 @@ int main(int argc, char *argv[])
 		}
 
         runTime.write();
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
 	}
 
     Info<< "End\n" << endl;
