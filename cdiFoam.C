ebb21f425ea3bae1157f31abdbc7ad1e2abc9b73
Ali Bozorgzadeh <aliiiib95@gmai.com>
2022-02-25 19:19:59 +0000

    feat(src terms): use of SuSp; encapsulate eqns...
    1. for cEqn and pDEqn, replace fully explicit source terms with
    fvm::SuSp() which is implicit/explicit depending on the situation. for
    pDEqn I linearize sinh(pD/VT) into first two linear terms of it's Taylor
    series expansion, and then applied the Picard's method to reach the
    final form of the source term.
    2. As each eqn now has coeficient fields stacked above the matrix
    object, I translate each equation into a dedicated file.

--- cdiFoam.C
+++ cdiFoam.C
@@ -60,71 +60,24 @@ int main(int argc, char *argv[])
     p.oldTime();
     pD.oldTime();
 
-    // // Define constant coefficients before the loop to only be calculated once
+    // Define constant coefficients before the loop to only be calculated once
     dimensionedScalar porosityRatio = (Pmi/Pma);
 
     while (simple.loop(runTime))
     {
         Info<< "Time = " << runTime.timeName() << nl << endl;
-
         #include "CourantNo.H"
 
-        // Electrical Potential (p)
-        fvScalarMatrix pEqn
-        (
-             - fvm::laplacian(c, p)
-             ==
-              (-1) * src_coeff_p    // This must be a positive term
-                   * porosityRatio
-                   * exp(Mu)
-                   * (VT/De)
-                   * (
-                         sinh(pD/VT) * fvc::ddt(c)
-                       + c * cosh(pD/VT) * fvc::ddt(pD) / VT
-                     )
-        );
+        // -- Electrical Potential (p) Equation
+        #include "pEqn.H"
 
-        pEqn.relax();
-        pEqn.solve();
-
-        dimensionedScalar dummy_coeff(1e-9);
-        volScalarField pD_coeff(2 * src_coeff_c * (F / CapSt) * (c / VT) * cosh(pD/VT));
-
-        // Donnan Potential (pD)
-        fvScalarMatrix pDEqn
-        (
-             fvm::ddt(1+pD_coeff,pD)
-             + src_coeff_c * dummy_coeff * fvm::laplacian(De,pD) // volt * m^2 / s / m^2
-             ==
-             src_coeff_c  *  dummy_coeff * fvc::laplacian(De,pD)
-             - fvc::ddt(p)
-             -2 * src_coeff_c     // This must be a negative term
-                  * F / CapSt * exp(Mu) * sinh(pD/VT) * fvc::ddt(c)
-        );
-
-        pDEqn.relax();
-        pDEqn.solve();
+        // -- Donnan Potential (pD) Equation
+        #include "pDEqn.H"
 
         while (simple.correctNonOrthogonal())
         {
-            volScalarField c_coeff(src_coeff_c * exp(Mu) * porosityRatio * cosh(pD/VT));
-
-            // Ion concentration (c)
-            fvScalarMatrix cEqn
-            (
-                 fvm::ddt(1+c_coeff,c)
-                 + coeff_conv * fvm::div(phi, c)
-                 - coeff_diff_1 * fvm::laplacian(De, c)         // spacer *2 : *0
-                 - coeff_diff_2 * fvm::laplacian(De, c)         // electrode *0 : *1
-                 ==
-                 (-1) * src_coeff_c * exp(Mu) * porosityRatio   // This must be a negative term
-                      * (
-                          c * sinh(pD/VT) * fvc::ddt(pD) / VT
-                        )
-            );
-
-            cEqn.relax();
-            cEqn.solve();
+            // -- Ion concentration (c) Equation
+            #include "cEqn.H"
         }
 
         runTime.write();
